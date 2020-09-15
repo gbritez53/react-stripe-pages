de0ba42166750b9d6a99a86d2755112f3453ae08
Gabriel Britez <gbritez53@gmail.com>
2020-09-15 19:00:40 -0300

    Updates

---
+++ precache-manifest.5debbca63286d850af2deb0ed9b998a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef02365dca8a070af59e4e021f9d91c",
+    "url": "/react-stripe-pages/index.html"
+  },
+  {
+    "revision": "3c979a18a6fd2c1efe3c",
+    "url": "/react-stripe-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd963ae485a67348a6c0",
+    "url": "/react-stripe-pages/static/js/2.4522203e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-stripe-pages/static/js/2.4522203e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c979a18a6fd2c1efe3c",
+    "url": "/react-stripe-pages/static/js/main.fc7dfa58.chunk.js"
+  },
+  {
+    "revision": "5b1607631aaa911ee462",
+    "url": "/react-stripe-pages/static/js/runtime-main.f0f396ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-stripe-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
